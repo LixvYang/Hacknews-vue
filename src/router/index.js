5e14ceca1bc1601a462faaeb50a051694e1f9e80
lixin yang <2690688423@qq.com>
2022-01-13 20:57:20 +0800

    add single

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../views/Home.vue'
+import Homepage from '../components/Homepage.vue'
+import Single from '../components/Single.vue'
+
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'Homepage',
+      component: Homepage,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ '../views/About.vue'),
+      path: '/story/:id',
+      name: 'Single',
+      component: Single,
     },
   ],
 })
